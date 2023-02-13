7e92c03db6c3f7aecc8aaa80b2bbfa1be7dea7f9
diego1277 <59052924+diego1277@users.noreply.github.com>
2023-02-13 15:15:21 -0300

    Update output.tf

--- output.tf
+++ output.tf
@@ -2,11 +2,18 @@ output "subnet_private_ids" {
   description = "private subnets id"
   value = aws_subnet.private[*].id
 }
-output subnet_public_ids {
+
+output "subnet_public_ids" {
   value       = aws_subnet.public[*].id
   description = "public subnets id"
 }
+
 output "vpc_id" {
   description = "vpc id"
   value = aws_vpc.this.id
+}
+
+output "default_sg_id" {
+  description = "default security group id"
+  value = aws_default_security_group.this.id
 }
