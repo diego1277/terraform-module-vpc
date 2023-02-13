6174325c452878e9d6805f7e5765a01d64bb2402
diego1277 <59052924+diego1277@users.noreply.github.com>
2023-02-13 14:56:01 -0300

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -2,5 +2,5 @@ resource "aws_vpc" "this" {
   cidr_block            = var.cidr_block
   enable_dns_hostnames  = var.enable_dns_hostnames
   enable_dns_support    = var.enable_dns_support
-  tags = local.common_tags
+  tags = merge({Name:"${var.name}"},local.common_tags)
 }
