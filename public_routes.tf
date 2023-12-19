447899aa940b0d708b88dd2d03699fd65bc81eee
diego1277 <59052924+diego1277@users.noreply.github.com>
2023-12-18 21:16:50 -0300

    Update public_routes.tf

--- public_routes.tf
+++ public_routes.tf
@@ -10,7 +10,7 @@ resource "aws_route" "public_internet_gateway" {
 
 resource "aws_route" "public_peering" {
   for_each                  = length(var.public_subnets) > 0 && length(var.public_routes_peering) > 0 ? var.public_routes_peering : {}
-  route_table_id            = aws_route_table.private[0].id
+  route_table_id            = aws_route_table.public.id
   destination_cidr_block    = each.value.destination_cidr_block
   vpc_peering_connection_id = each.value.vpc_peering_connection_id
 }
